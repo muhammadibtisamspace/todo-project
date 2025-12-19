718d2f502fc056c0fababedc55e1f32fe0ef00e7
muhammad ibtisam <muhammadibtisamspace@gmail.com>
2025-12-19 15:04:57 +0500

    added basic functionality and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-function App() {
-  const getText = null;
+import { useState } from "react";
 
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  // Functions To Handle All The Logic & Events
   const InputHandler = (event) => {
-    console.log(event.target.value);
-    
-  }
+    setInputText(event.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    setTodos([...todos, inputText]);
+    setInputText("");
+  };
 
   return (
     <>
@@ -14,22 +23,29 @@ function App() {
         </h1>
       </header>
 
-      <main className="justify-center items-center flex mt-4">
-        {/* Your main application content goes here */}
+      <form action="">
+        <main className="justify-center items-center flex mt-4">
+          {/* Your main application content goes here */}
 
-        {/* Search Input */}
-        <div>
-          <input
-            type="text"
-            className="border border-gray-300  px-4 py-2 rounded-2xl bg-white text-black h-12 w-104 font-semibold text-center"
-            placeholder="Enter Your Task"
-            onChange={InputHandler}
-          />
-          <button className="border h-12 w-26 rounded-2xl bg-violet-800 font-bold fon hover:cursor-pointer">
-            Submit
-          </button>
-        </div>
-      </main>
+          {/* Search Input */}
+          <div>
+            <input
+              type="text"
+              className="border border-gray-300  px-4 py-2 rounded-2xl bg-white text-black h-12 w-104 font-semibold text-center"
+              placeholder="Enter Your Task"
+              value={inputText}
+              onChange={InputHandler}
+            />
+            <button
+              className="border h-12 w-26 rounded-2xl bg-violet-800 font-bold fon hover:cursor-pointer"
+              type="submit"
+              onClick={onSubmitHandler}
+            >
+              Submit
+            </button>
+          </div>
+        </main>
+      </form>
 
       {/* Showing Input of lst added */}
 
@@ -38,12 +54,20 @@ function App() {
           <h2 className="text-3xl font-semibold justify-center items-center">
             Your Todos
           </h2>
-          <div className="h-168 w-264 bg-white rounded-3xl mt-4 text-black p-4 text-start gap-4 justify-center items-center">
-            <p>{getText}</p>
 
-            <button className="bg-green-500">Move Up</button>
-            <button className="bg-amber-700">Move down</button>
-            <button className="bg-red-500">Delete</button>
+          <div>
+            <ul className="">
+              {todos.map((todo, i) => (
+                <>
+                  <li key={i}>
+                    {todo}
+                    <button className="bg-green-500">Move Up</button>
+                    <button className="bg-amber-700">Move down</button>
+                    <button className="bg-red-500">Delete</button>
+                  </li>
+                </>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
